ea19684de1647606ca4308906000636a81776526
Jamie Falconer <mrfluffyoven@gmail.com>
2025-09-24 17:17:50 +0100

    test: tw uses recovery as boot

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,7 @@ TW_INCLUDE_REPACKTOOLS := true
 TW_INCLUDE_LPTOOLS := true
 TW_EXCLUDE_APEX := true
 TW_EXTRA_LANGUAGES := true
+BOARD_USES_RECOVERY_AS_BOOT := true
 
 # Making the recovery.img smaller
 BOARD_HAS_NO_REAL_SDCARD := true
