dce0a504b3f142aa341b4b17819727ac4bcc2afb
Jamie Falconer <mrfluffyoven@gmail.com>
2025-09-24 17:04:26 +0100

    update dtb path (Again)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,10 +43,10 @@ BOARD_KERNEL_IMAGE_NAME := Image
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
-TARGET_PREBUILT_DTB := device/samsung/a04s/prebuilt/dtb.img
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-BOARD_MKBOOTIMG_ARGS += --dtb device/samsung/a04s/prebuilt/dtb.img
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_MKBOOTIMG_ARGS += --dtb $(DEVICE_PATH)/prebuilt/dtb.img
+#BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
