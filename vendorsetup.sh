8f25a0230c7e5292fd32e06dfe5bfa022cc73c27
Jamie Falconer <mrfluffyoven@gmail.com>
2025-11-29 13:34:04 +0000

    cleanup vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,2 @@
 export ALLOW_MISSING_DEPENDENCIES=true
-git clone https://gitlab.com/HayateDevTH/avb_tool -b main out/host/linux-x86/bin
-sudo chmod +rwx out/host/linux-x86/bin/avbtool
 chmod a+x device/samsung/a12s/mkbootimg
