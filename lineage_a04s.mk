5574b980eff0b37010de8000ec382c8876298171
Michael <michaelowusuntim6@gmail.com>
2026-03-19 05:59:02 +0000

    Update lineage_a04s.mk

--- lineage_a04s.mk
+++ lineage_a04s.mk
@@ -1,18 +1,21 @@
-#
-# SPDX-FileCopyrightText: The LineageOS Project
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from a04s device
+# Inherit from a04s device configuration
 $(call inherit-product, device/samsung/a04s/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit common Lineage/Rising stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# RisingOS Specific Flags
+RISING_MAINTAINER := Mike
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_USES_PICO_GAPPS := true
+TARGET_ENABLE_BLUR := false  # Set to false for 3GB RAM performance
+
+# Device Identity
 PRODUCT_DEVICE := a04s
 PRODUCT_NAME := lineage_a04s
 PRODUCT_BRAND := samsung
@@ -21,6 +24,8 @@ PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
 
+# Build Properties
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="a04snnxx-user 14 UP1A.231005.007 A047FXXSDEYL1 release-keys" \
-    BuildFingerprint=samsung/a04snnxx/a04s:14/UP1A.231005.007/A047FXXSDEYL1:user/release-keys
+    RisingChipset="Exynos 850" \
+    RisingMaintainer="Mike" \
+    BuildDesc="a04snnxx-user 14 UP1A.231005.007 A047FXXSDEYL1 release-keys"
